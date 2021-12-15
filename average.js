9fe84dc7f2d870e7e76cf749fdcc74eba6965412
Vamshi Addanki <vamshi.a4u@gmail.com>
2021-12-16 00:20:38 +0530

    added outlier identification

--- average.js
+++ average.js
@@ -1,7 +1,54 @@
+function splice(a,s,e){
+  var t=[]
+  for(var i=s;i<e;i++){
+    t.push(a[i])
+  }
+  return t
+}
+
+function divide(a){
+    var n=a.length
+    var ex=(n%2==0)?0:1
+    var m=Math.floor(n/2)
+    return [splice(a,0,m),splice(a,m+ex,n)]
+}
+
+function sort(v){
+  var a=v
+  var t;
+  for(var i=0;i<a.length;i++){
+     for(var j=0;j<a.length-i-1;j++){
+          if(a[j]>a[j+1]){
+            t=a[j]
+            a[j]=a[j+1]
+            a[j+1]=t
+          }
+     }
+  }
+  return a
+}
+
+function median(a){
+  var n=a.length
+  return (n%2==0) ? (a[n/2]+a[(n/2)-1])/2 : a[parseInt(n/2)]
+}
+
+function iqr(v){
+  var a=sort(v)
+  var [q1,q3]=divide(a)
+  return [median(q3)-median(q1),median(q1),median(q3)]
+}
+
+function threshold(v){
+  var [iq,q1,q3]=iqr(v)
+  return [q1-(1.5*iq),q3+(1.5*iq)]
+}
 
 function average(numbers) {
   numbers=numbers.filter(number=>!Number.isNaN(number))
+  var [lower,upper] = threshold(numbers)
+  numbers=numbers.filter(number=>lower<=number && number<=upper)
   return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
 }
 
-module.exports = {average};
+module.exports = {average,threshold};
--- test/average.test.js
+++ test/average.test.js
@@ -1,5 +1,5 @@
 const {expect} = require('chai');
-const {average} = require('../average');
+const {average, threshold} = require('../average');
 
 it('computes average of a list of numbers', ()=> {
   // floating point numbers cannot be compared for equality,
@@ -13,4 +13,20 @@ it('reports the average as NaN on an empty list', ()=> {
 
 it('ignores NaN in the input', ()=> {
   expect(average([1, NaN, 2])).to.be.approximately(1.5, 0.01);
+});
+
+it('testing lower bound', ()=> {
+  expect(threshold([30, 30, 32, 35, 38, 38, 38, 39, 39, 40, 40, 40, 41, 42, 42, 42, 44, 45, 50, 55])[0]).equal(32)
+});
+
+it('testing upper bound', ()=> {
+  expect(threshold([30, 30, 32, 35, 38, 38, 38, 39, 39, 40, 40, 40, 41, 42, 42, 42, 44, 45, 50, 55])[1]).equal(48)
+});
+
+it('ignores outliers in the input', ()=> {
+  expect(average([30, 30, 32, 35, 38, 38, 38, 39, 39, 40, 40, 40, 41, 42, 42, 42, 44, 45, 50, 55])).to.be.approximately(39.6, 0.1);
+});
+
+it('ignores outliers in the input', ()=> {
+  expect(average([30,31,31,32,33,35,48,56,62,70,88,130])).to.be.approximately(46.9, 0.1);
 });
